0862b7f665f8ca22f79015b93da34287b45bf23e
narendrapatidar <nk580585@gmail.com>
2022-05-24 09:41:08 +0530

    with EWS

--- src/Controllers/bookControllers.js
+++ src/Controllers/bookControllers.js
@@ -44,9 +44,7 @@ let uploadFile= async ( file) =>{
          return resolve(data.Location)
      })
  
-     // let data= await s3.upload( uploadParams)
-     // if( data) return data.Location
-     // else return "there is an error"
+    
  
     })
  }
@@ -56,13 +54,13 @@ const createBook = async function (req, res) {
     try {
 
         let data = req.body
-    let userId = data.userId
+  
         console.log(data)
         
-        // let userId = data.userId
-        // if (!isValidObjectId(userId)) {
-        //     return res.status(400).send({ status: false, msg: `${userId} invalid userId` })
-        // }
+        let userId = data.userId
+        if (!isValidObjectId(userId)) {
+            return res.status(400).send({ status: false, msg: `${userId} invalid userId` })
+        }
         let checkuser = await userModels.findOne({_id:data.userId})
         console.log(checkuser)
 
@@ -71,9 +69,9 @@ const createBook = async function (req, res) {
 
         }
 
-        // if (userId != req.userId) {
-        //     return res.status(403).send({ status: false, msg: "you can't create the book " })
-        // }
+        if (userId != req.userId) {
+            return res.status(403).send({ status: false, msg: "you can't create the book " })
+        }
 
         if (!isValidRequestBody(data)) {
             return res.send({ status: false, msg: "please provide  details" })
@@ -114,55 +112,7 @@ const createBook = async function (req, res) {
                 region: "ap-south-1"
             })
             
-            // let uploadFile= async ( file) =>{
-            //    return new Promise( function(resolve, reject) {
-            //     // this function will upload file to aws and return the link
-            //     let s3= new aws.S3({apiVersion: '2006-03-01'}); // we will be using the s3 service of aws
             
-            //     var uploadParams= {
-            //         ACL: "public-read",
-            //         Bucket: "classroom-training-bucket",  //HERE
-            //         Key: "abc/" + file.originalname, //HERE 
-            //         Body: file.buffer
-            //     }
-            
-            
-            //     s3.upload( uploadParams, function (err, data ){
-            //         if(err) {
-            //             return reject({"error": err})
-            //         }
-            //         console.log(data)
-            //         console.log("file uploaded succesfully")
-            //         return resolve(data.Location)
-            //     })
-            
-            //     // let data= await s3.upload( uploadParams)
-            //     // if( data) return data.Location
-            //     // else return "there is an error"
-            
-            //    })
-            // }
-            
-            // router.post("/write-file-aws", async function(req, res){
-            
-            //     try{
-            //         let files= req.files
-            //         if(files && files.length>0){
-            //             //upload to s3 and get the uploaded link
-            //             // res.send the link back to frontend/postman
-            //             let uploadedFileURL= await uploadFile( files[0] )
-            //             res.status(201).send({msg: "file uploaded succesfully", data: uploadedFileURL})
-            //         }
-            //         else{
-            //             res.status(400).send({ msg: "No file found" })
-            //         }
-                    
-            //     }
-            //     catch(err){
-            //         res.status(500).send({msg: err})
-            //     }
-                
-            // })
             
           
             
@@ -201,8 +151,6 @@ const createBook = async function (req, res) {
         if(!(files && files.length>0)){
 
         }
-            //upload to s3 and get the uploaded link
-            // res.send the link back to frontend/postman
             let uploadedFileURL= await uploadFile( files[0] )
            data.bookCover=uploadedFileURL
 
